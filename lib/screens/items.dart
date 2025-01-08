8e9e2d30baaeb3a8655693fe381ae3e2c29f529b
= <bojan.vasilevski@students.finki.ukim.mk>
2025-01-08 12:54:42 +0100

    Changed items listing and added few more items

--- lib/screens/items.dart
+++ lib/screens/items.dart
@@ -10,11 +10,15 @@ class ItemsScreen extends StatefulWidget {
 }
 
 class _ItemsScreenState extends State<ItemsScreen> {
-  // List of categories
-  final List<String> categories = ['CPU', 'GPU', 'PSU', 'Storage'];
+  final List<String> categories = [
+    'CPU',
+    'GPU',
+    'PSU',
+    'Storage',
+    'Motherboard'
+  ];
   int _selectedIndex = 0;
 
-  // Map of products for each category
   final Map<String, List<Map<String, dynamic>>> products = {
     'CPU': [
       {
@@ -35,8 +39,8 @@ class _ItemsScreenState extends State<ItemsScreen> {
         'price': '\$100',
         'image':
             'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbARdid9zKeXZugWMinxvCvjkCLDi-jN7axA&s',
-        'specifications': '4 cores, 4 threads, 3.0 GHz base  clock'
-      }
+        'specifications': '4 cores, 4 threads, 3.0 GHz base clock',
+      },
     ],
     'GPU': [
       {
@@ -53,6 +57,30 @@ class _ItemsScreenState extends State<ItemsScreen> {
         'specifications': '16 GB GDDR6, 3840 stream processors',
       },
     ],
+    'PSU': [
+      {
+        'name': 'GEMBIRD 700W',
+        'price': '\$50',
+        'image': 'https://www.e-shop.gr/images/PER/BIG/PER.585367.jpg',
+        'specifications': '700W PSU'
+      }
+    ],
+    'Storage': [
+      {
+        'name': 'Samsung NVME M.2 1TB',
+        'price': '\$100',
+        'image': 'https://www.insomnia.mk/files/resized/products/47865_0-1000x1000.1800x1800w.png',
+        'specifications': '1TB Super fast storage'
+      }
+    ],
+    'Motherboard': [
+      {
+        'name': 'MSI H110M PRO-D',
+        'price': '\$50',
+        'image': 'https://m.media-amazon.com/images/I/71FJ9jPc2xL._AC_UF894,1000_QL80_.jpg',
+        'specifications': 'Intel socket DDR4 motherboard'
+      }
+    ]
   };
 
   void _onIndexChanged(int index) {
@@ -66,7 +94,8 @@ class _ItemsScreenState extends State<ItemsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // Filter products based on search query
+    final screenSize = MediaQuery.of(context).size;
+
     List<Map<String, dynamic>> filteredProducts = products[selectedCategory]!
         .where((product) =>
             product['name'].toLowerCase().contains(searchQuery.toLowerCase()))
@@ -78,9 +107,8 @@ class _ItemsScreenState extends State<ItemsScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(height: 16),
-          // Top padding
 
-          // Search bar above categories
+          // Search bar
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: TextField(
@@ -107,11 +135,10 @@ class _ItemsScreenState extends State<ItemsScreen> {
           ),
 
           const SizedBox(height: 16),
-          // Spacing between search bar and categories
 
-          // Categories list (horizontally scrollable)
+          // Categories list
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 100),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
             child: Center(
               child: SizedBox(
                 height: 50,
@@ -158,28 +185,26 @@ class _ItemsScreenState extends State<ItemsScreen> {
           ),
           const SizedBox(height: 16),
 
-          // Products grid for selected category
+          // Products grid
           Expanded(
             child: GridView.builder(
               padding: const EdgeInsets.all(12),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: screenSize.width > 600 ? 3 : 2,
                 crossAxisSpacing: 8,
                 mainAxisSpacing: 8,
-                childAspectRatio: 1, // Adjusted aspect ratio for smaller cards
+                childAspectRatio: screenSize.width / (screenSize.height * 0.8),
               ),
               itemCount: filteredProducts.length,
               itemBuilder: (context, index) {
                 final product = filteredProducts[index];
                 return GestureDetector(
                   onTap: () {
-                    // Navigate to the ItemDetailsScreen when tapping on the card
                     Navigator.push(
                       context,
                       MaterialPageRoute(
                         builder: (context) => ItemDetailsScreen(
-                          product:
-                              product, // Passing the selected product to the details screen
+                          product: product,
                         ),
                       ),
                     );
@@ -194,7 +219,7 @@ class _ItemsScreenState extends State<ItemsScreen> {
                       children: [
                         // Product image
                         Container(
-                          height: 150, // Reduced image height
+                          height: screenSize.height * 0.2,
                           decoration: BoxDecoration(
                             borderRadius: const BorderRadius.only(
                               topLeft: Radius.circular(12),
@@ -206,26 +231,28 @@ class _ItemsScreenState extends State<ItemsScreen> {
                             ),
                           ),
                         ),
-                        const SizedBox(height: 6), // Reduced spacing
+                        const SizedBox(height: 6),
 
                         // Product name
                         Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8),
-                            child: Center(
-                              child: Text(
-                                product['name'],
-                                style: const TextStyle(
-                                  fontSize: 14, // Reduced font size
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.blueAccent,
-                                ),
-                                maxLines: 1,
-                                overflow: TextOverflow.ellipsis,
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: Center(
+                            child: Text(
+                              product['name'],
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blueAccent,
                               ),
-                            )),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+
+                        const Spacer(),
 
                         // Product price and Add to Cart button
-                        const Spacer(), // Push content to the bottom
                         Padding(
                           padding: const EdgeInsets.symmetric(
                               horizontal: 8, vertical: 6),
@@ -235,21 +262,20 @@ class _ItemsScreenState extends State<ItemsScreen> {
                               Text(
                                 product['price'],
                                 style: const TextStyle(
-                                  fontSize: 14, // Reduced font size
+                                  fontSize: 14,
                                   fontWeight: FontWeight.bold,
                                   color: Colors.green,
                                 ),
                               ),
                               ElevatedButton(
                                 onPressed: () {
-                                  // Add to Cart functionality goes here
+                                  // Add to Cart functionality
                                 },
                                 style: ElevatedButton.styleFrom(
                                   backgroundColor: Colors.orange,
                                   padding: const EdgeInsets.symmetric(
                                       horizontal: 8, vertical: 4),
-                                  textStyle: const TextStyle(
-                                      fontSize: 12), // Reduced button font size
+                                  textStyle: const TextStyle(fontSize: 12),
                                 ),
                                 child: const Text('Add to cart'),
                               ),
@@ -263,14 +289,15 @@ class _ItemsScreenState extends State<ItemsScreen> {
               },
             ),
           ),
-          // Navigation bar at the bottom
+
+          // Navigation bar
           Align(
             alignment: Alignment.bottomCenter,
             child: NavigationWidget(
               onIndexChanged: _onIndexChanged,
               selectedIndex: _selectedIndex,
             ),
-          )
+          ),
         ],
       ),
     );
